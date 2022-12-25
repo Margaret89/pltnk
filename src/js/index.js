c7c093c5683f1b1a60e3b7ffc7c8e3b992f86922
Margaret <match14@yandex.ru>
2022-12-25 10:38:33 +0300

    header + footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 import {$} from './common';
 
-$(window).scroll(function(){
+// Плавный переход к ссылке
+if ($('.js-link-move').length) {
+	$('body').on('click','.js-link-move', function (event) {
+		event.preventDefault();
+		const thisPos=event.target.getBoundingClientRect().top+window.scrollY;
+		const id  = $(this).attr('href');
+		const top =document.getElementById(id.substr(1)).getBoundingClientRect().top+window.scrollY-80;
+		const speed = Math.round((top-thisPos)/15)+400;
+		$('body,html').animate({scrollTop: top}, speed);
+	});
+}
+
+// Стрелка наверх
+$(window).on('scroll', function(){
 	if($(this).scrollTop()>300){
 		$('.js-move-up').addClass('visible');
 	}else{
 		$('.js-move-up').removeClass('visible');
 	}
 });
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+$('.js-move-up').on('click', function(){$('body,html').animate({scrollTop:0},500);return false;});
